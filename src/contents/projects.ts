db86f884d2c8d7fa1794512943993ecd00f4f4b6
tanushka <tanushka1726@gmail.com>
2025-10-28 13:17:31 +0530

    edit demolink

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -2,43 +2,43 @@ import { Project } from "@/types";
 
 
 export const projects: Project[] = [
-    {
-        title: 'Readers Platform',
-        description: 'A Readers platform built with Next.js, TypeScript, and Tailwind CSS.',
-        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS'],
-        githubLink: 'https://github.com/tanushka1726/react_next.git',
-        demoLink: 'https://readerhere.netlify.app/',
-        image: '/projects/Reader_website.jpeg'
-      },
-     
-      {
-        title: 'Portfolio Website',
-        description: 'My personal portfolio website showcasing my projects and skills.',
-        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-        githubLink: 'https://github.com/tanushka1726/Portfolio.git',
-        demoLink: 'https://portfolio-l9fm.onrender.com',
-        image: '/projects/Portfolio.png',
-      },
+  {
+    title: 'Readers Platform',
+    description: 'A Readers platform built with Next.js, TypeScript, and Tailwind CSS.',
+    technologies: ['Next.js', 'TypeScript', 'Tailwind CSS'],
+    githubLink: 'https://github.com/tanushka1726/react_next.git',
+    demoLink: 'https://readerhere.netlify.app/',
+    image: '/projects/Reader_website.jpeg'
+  },
 
-        {
-        title: 'An AI-Virtual Assistant',
-        description: 'An virtual assisant which search dynamically on google by the voice command',
-        technologies: ["HTML" , "CSS" , "Javascript"],
-        githubLink: 'https://github.com/tanushka1726/AI-virtual-assistant.git',
-        demoLink: 'https://tanushka1726.github.io/AI-virtual-assistant/',
-        image: '/projects/AI-virtual.png',
-      },
-      {
-        title: 'An Interview-Prep-Ai',
-        description: 'AI-based Interview Preparation Platform built using the MERN stack.',
-        technologies: ["React.js","Express.js","Node.js","MongoDB"],
-        githubLink: 'https://github.com/tanushka1726/Interview-Prep-Ai.git',
-        demoLink: 'https://tanushka1726.github.io/AI-virtual-assistant/',
-        image: '/projects/Interview-Prep.png',
-      },
-      
-      
-      
-      
-      
-  ];
+  {
+    title: 'Portfolio Website',
+    description: 'My personal portfolio website showcasing my projects and skills.',
+    technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+    githubLink: 'https://github.com/tanushka1726/Portfolio.git',
+    demoLink: 'https://portfolio-l9fm.onrender.com',
+    image: '/projects/Portfolio.png',
+  },
+
+  {
+    title: 'An AI-Virtual Assistant',
+    description: 'An virtual assisant which search dynamically on google by the voice command',
+    technologies: ["HTML", "CSS", "Javascript"],
+    githubLink: 'https://github.com/tanushka1726/AI-virtual-assistant.git',
+    demoLink: 'https://tanushka1726.github.io/AI-virtual-assistant/',
+    image: '/projects/AI-virtual.png',
+  },
+  {
+    title: 'An Interview-Prep-Ai',
+    description: 'AI-based Interview Preparation Platform built using the MERN stack.',
+    technologies: ["React.js", "Express.js", "Node.js", "MongoDB"],
+    githubLink: 'https://github.com/tanushka1726/Interview-Prep-Ai.git',
+    demoLink: 'https://interview-prep-ai-1-q9kt.onrender.com/',
+    image: '/projects/Interview-Prep.png',
+  },
+
+
+
+
+
+];
